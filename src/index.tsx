1f39d69e13a228936585c029ed7af1408fa57376
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-02 23:36:50 -0600

    Revert "Doggo"
    This reverts commit f26f51a948ef27cc594f6c30630d538fc7fabd8d.

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import axios from 'axios'
 
-const fetch: string = 'https://random.dog/woof.json'
+
 function App() {
-
-  const [source, setSource] = useState<string>('')
-
-  function generate() {
-    axios.get(fetch).then(
-      res => {
-        setSource(res.data.url)
-      })
-  }
-  function File() {
-    const fileType = source.substr(source.length - 3);
-    if (fileType === 'mp4') {
-      return <video onClick={generate} className='dog-image' autoPlay loop muted>
-        <source src={source} type='video/mp4' />
-      </video>
-    }
-    return <img onClick={generate} src={source} className='dog-image' alt='loading... maybe' />
-  }
-  window.onload = function () {
-    generate()
-  }
   return (
-    <div className='home'>
-      <File />
-      <p onClick={generate} className='button'>More Doggos</p>
+    <div>
+
     </div>
   );
 }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
